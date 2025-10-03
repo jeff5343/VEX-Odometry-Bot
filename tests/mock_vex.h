529c8f43ff33416897ec756435e56bc522e231fa
Jeff <90739298+jeff5343@users.noreply.github.com>
2025-10-04 08:37:58 +0900

    Update odometry in thread (#3)
    * Fix spelling of angled
    
    * Try adding threading NOT WORKING
    
    * Fix odom tests for thread
    
    * Add clarifying comment

--- tests/mock_vex.h
+++ tests/mock_vex.h
@@ -1,6 +1,10 @@
 #ifndef __MOCK_VEX_H_INCLUDED__
 #define __MOCK_VEX_H_INCLUDED__
 
+#include <thread>
+#include <mutex>
+#include <chrono>
+
 namespace vex
 {
     enum rotationUnits
@@ -23,6 +27,17 @@ namespace vex
             mockRevs = revs;
         }
     };
+
+    // Mock VEX threading (errors can be ignored)
+    using thread = std::thread;
+    using mutex = std::mutex;
+
+    namespace this_thread {
+        inline void sleep_for(int milliseconds) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(milliseconds));
+        }
+    }
+
 }
 
 #endif
