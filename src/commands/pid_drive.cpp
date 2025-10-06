5387f9177c0d047a372f142d1c18f124ee45fe8a
Jeff <hongjefferson211@gmail.com>
2025-10-06 16:58:06 -0700

    PidDrive turns directly to target then drives straight

--- src/commands/pid_drive.cpp
+++ src/commands/pid_drive.cpp
@@ -3,26 +3,40 @@
 void PidDrive::setTargetPose(Pose pose)
 {
     target = pose;
-    anglePid.setSetpoint(pose.radians);
+    straightPid.setSetpoint(0);
+    anglePid.setSetpoint(0);
 }
 
 void PidDrive::update()
 {
     Pose current = drivetrain.getPose();
 
-    if (!anglePid.isAtSetpoint())
-    {
-        double turnOutput = anglePid.calculate(current.radians);
-        drivetrain.setPercentOut(-turnOutput, turnOutput);
-        return;
-    }
-
     double errorX = current.x - target.x;
     double errorY = current.y - target.y;
     double errorDist = sqrt(errorX * errorX + errorY * errorY);
+    double errorAngle = atan2(errorY, errorX);
 
-    double straightOutput = anglePid.calculate(errorDist);
-    drivetrain.setPercentOut(straightOutput, straightOutput);
+    // PID based on error to target (setpoints are 0)
+    double straightOutput = -straightPid.calculate(errorDist);
+    double turnOutput = anglePid.calculate(errorAngle);
+
+    // moving to pose
+    if (!straightPid.isAtSetpoint())
+    {
+        // first point to pose
+        if (!anglePid.isAtSetpoint())
+        {
+            drivetrain.setPercentOut(-turnOutput, turnOutput);
+            return;
+        }
+        // drive to pose
+        drivetrain.setPercentOut(straightOutput, straightOutput);
+    }
+
+    // TODO: add rotating to pose defined angle at the end!
+    // if (anglePid.isAtSetpoint()) {
+    //     anglePid.setSetpoint(target.radians);
+    // }
 }
 
 bool PidDrive::isAtSetpoint()
